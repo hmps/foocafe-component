394299e69a724cac2537c1e68672756f281fe989
Hampus Persson <h@hmps.se>
2015-09-03 13:21:14 +0200

    update package.json

--- jspm.conf.js
+++ jspm.conf.js
@@ -17,6 +17,7 @@ System.config({
     "babel": "npm:babel-core@5.8.23",
     "babel-runtime": "npm:babel-runtime@5.8.20",
     "core-js": "npm:core-js@1.1.3",
+    "text": "github:systemjs/plugin-text@0.0.2",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
